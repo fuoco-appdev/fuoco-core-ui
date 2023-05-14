527cf5e52dde80730217fecf8a6581386efb3f5d
Lucas Fuoco <fuoco.appdev@gmail.com>
2023-05-14 11:03:34 -0400

    Fix card

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,39 +1,58 @@
 import React from 'react'
 // @ts-ignore
 import CardStyles from './card.module.scss'
+import Ripples, { RipplesProps } from 'react-ripples'
+
+export interface CardClasses {
+  container?: string
+  card?: string
+  hoverable?: string
+  clickable?: string
+}
 
 export interface CardProps {
   ref?: React.RefObject<HTMLDivElement>
   key?: React.Key
   children?: React.ReactNode
-  className?: string
+  classNames?: CardClasses
   hoverable?: boolean
   clickable?: boolean
   style?: React.CSSProperties
+  rippleProps?: RipplesProps
   onClick?: (event: React.MouseEvent<HTMLDivElement>) => void
 }
 
 function Card(
-  { key, children, className, hoverable, clickable, style, onClick }: CardProps,
+  {
+    key,
+    children,
+    classNames,
+    hoverable,
+    clickable,
+    style,
+    rippleProps,
+    onClick,
+  }: CardProps,
   ref: React.ForwardedRef<any>
 ) {
-  let classes = [CardStyles['card']]
-  if (hoverable) classes.push(CardStyles['card-hoverable'])
+  let containerClasses = [CardStyles['button-ripple'], classNames?.container]
+  let classes = [CardStyles['card'], classNames?.card]
+  if (hoverable)
+    classes.push(CardStyles['card-hoverable'], classNames?.hoverable)
   if (clickable) {
-    classes.push(CardStyles['card-clickable'])
+    classes.push(CardStyles['card-clickable'], classNames?.clickable)
   }
-  if (className) classes.push(className)
 
   return (
-    <div
-      ref={ref}
-      key={key}
-      className={classes.join(' ')}
-      style={style}
+    <Ripples
+      {...(clickable ? rippleProps : { color: 'rgba(0,0,0,0)' })}
+      className={containerClasses.join(' ')}
       onClick={onClick}
     >
-      {children}
-    </div>
+      <div ref={ref} key={key} className={classes.join(' ')} style={style}>
+        {children}
+      </div>
+    </Ripples>
   )
 }
 
