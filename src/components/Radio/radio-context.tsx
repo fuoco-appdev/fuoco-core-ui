fcb148ed32ee070ad11673bbec9271a349ef77d3
Lucas Fuoco <fuoco.appdev@gmail.com>
2023-11-09 14:58:33 -0500

    Fix checkbox style

--- src/components/Radio/radio-context.tsx
+++ src/components/Radio/radio-context.tsx
@@ -2,7 +2,13 @@ import { createContext } from 'react'
 
 // Make sure the shape of the default value passed to
 // createContext matches the shape that the consumers expect!
-export const RadioContext = createContext({
+export const RadioContext = createContext<{
+  parentCallback: (e: any) => void
+  type: string
+  name: string
+  activeId: string
+  parentSize: string
+}>({
   parentCallback: (e: any) => {},
   type: '',
   name: '',
